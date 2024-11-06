d489e1b5f6ed58e2d62a5646e15132ec9d87d4b2
Adenilson Ribeiro <134528770+aaadjrbr@users.noreply.github.com>
2024-11-06 08:58:06 -0300

    styling-changing-bg1

--- public/scripts/chat2.js
+++ public/scripts/chat2.js
@@ -9,7 +9,7 @@ const kidId = urlParams.get('kidId'); // kidId passed as a URL parameter
 let parentId;
 
 // Placeholder for the default background image
-const originalBgUrl = 'https://firebasestorage.googleapis.com/v0/b/kids-chatgpt.appspot.com/o/static%2Falfie-chat-bg1.webp?alt=media&token=67620371-374a-42f2-9ac6-c271e3ff6e18';
+const originalBgUrl = 'https://firebasestorage.googleapis.com/v0/b/kids-chatgpt.appspot.com/o/static%2Falfie-chat-bg1.webp?alt=media&token=e6284b91-c68f-4af9-96bb-6753f224f9db';
 const auth = getAuth();
 const storage = getStorage();
 
